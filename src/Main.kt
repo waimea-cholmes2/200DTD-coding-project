ade897703826d0623a819ef22c6d8da3ae208b05
waimea-cholmes2 <cholmes2@waimea.school.nz>
2024-10-21 10:55:15 +1300

    did stuff, still dontr get iyt

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,9 @@
 /**
  * ------------------------------------------------------------------------
- * PROJECT NAME HERE
+ * Old Gold
  * Level 2 programming project
  *
- * by YOUR NAME HERE
+ * By Cooper Holmes
  *
  * BRIEF PROJECT DESCRIPTION HERE
  * BRIEF PROJECT DESCRIPTION HERE
@@ -16,6 +16,38 @@
  * Program entry point
  */
 fun main() {
-    println("Hello World!")
+    // show Title
+    println("-------------------")
+    println("WELCOME TO OLD GOLD")
+    println("-------------------")
+
+    val coins = mutableListOf <String>()
+    coins.add("C")
+    coins.add("C")
+    coins.add("C")
+    coins.add("C")
+    coins.add("G")
+
+    getUserNames()
+
+    generateCoins()
+
+    generateGameBase()
 }
 
+fun getUserNames() {
+    print("Enter Player 1's Name:")
+    val player1 = readln()
+
+    print("Enter Player 2's Name:")
+    val player2 = readln()
+}
+
+fun generateCoins() {
+println()
+}
+
+fun generateGameBase() {
+    println("-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --")
+    println("1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20")
+}
