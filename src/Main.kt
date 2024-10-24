d80d6b722a1f3ab63ac75b9681f407601b09ef44
waimea-cholmes2 <cholmes2@waimea.school.nz>
2024-10-24 14:51:41 +1300

    did lots

--- src/Main.kt
+++ src/Main.kt
@@ -20,9 +20,9 @@ import kotlin.random.Random
 
 fun main() {
     // show Title
-    println("-------------------")
-    println("WELCOME TO OLD GOLD")
-    println("-------------------")
+    println("----------------------------------")
+    println("       WELCOME TO OLD GOLD")
+    println("----------------------------------")
 
     // Make the coin list
     val coins = MutableList(20) { " " }
@@ -38,22 +38,42 @@ fun main() {
         coins[position] = coin // Place the coin
     }
 
-    getUserNames()
+    // Get the user's names and welcome them
+    val (player1, player2) = getUserNames()
 
-    generateGameBase(coins)
+    // Randomly select a player to start
+    var currentPlayer = if (Random.nextBoolean()) player1 else player2
+
+    //Start the game
+    var game = false
+    while (!game) {
+        println("\n$currentPlayer's turn!")
+
+        // Generate the board
+        generateGameBase(coins)
+
+        // Start the players turns
+        game = playerMove(currentPlayer, coins)
+
+        // Swap players after each turn
+        currentPlayer = if (currentPlayer == player1) player2 else player1
+    }
 }
 
-fun getUserNames() {
+fun getUserNames(): Pair<String, String> {
     print("Enter Player 1's Name: ")
-    val player1 = readln()
+    val player1 = readln().capitalize()
 
     print("Enter Player 2's Name: ")
-    val player2 = readln()
+    val player2 = readln().capitalize()
+
+    println("Welcome $player1 and $player2")
+    return Pair(player1, player2)
 }
 
 fun generateGameBase(coins: MutableList<String>) {
     // Build the top
-    var x=1
+    var x = 1
     print("+---".repeat(coins.size))
     println("+")
 
@@ -67,14 +87,64 @@ fun generateGameBase(coins: MutableList<String>) {
     print("+---".repeat(coins.size))
     println("+")
 
-    // Display the numbers
+    // Display the numbers below the grid
     for (i in coins.indices) {
         print("|%-2d ".format(x))
         x++
     }
     println("|")
 
-    //Build the bottom
+    // Build the bottom
     print("+---".repeat(coins.size))
     println("+")
+}
+
+fun playerMove(currentPlayer: String, coins: MutableList<String>): Boolean {
+    var validMove = false
+    while (!validMove) {
+        print("Enter the position of the coin you'd like to move or remove: ")
+        val position = readln().toInt() - 1
+
+        if (position !in coins.indices || coins[position] == " ") {
+            println("Invalid move. Please select a square with a coin.")
+            continue
+        }
+
+        if (position == 0) {
+            // Check if it's in position 1 and remove it
+            println("$currentPlayer has removed a coin")
+            coins[0] = " "
+        }
+        else {
+            // Ask player which square they want to move the coin to
+            print("Enter the square number to move the coin to: ")
+            val targetSquare = readln().toInt() - 1
+
+            // does the move
+            validMove = moveCoin(coins, position, targetSquare)
+        }
+    }
+
+    return false
+}
+
+fun moveCoin(coins: MutableList<String>, position: Int, targetSquare: Int): Boolean {
+    // Check if the square has no coin in it already and to the left of the coin's current position
+    if (targetSquare < 0 || targetSquare >= position || coins[targetSquare] != " ") {
+        println("Invalid move. You can only move the coin to an empty square on the left.")
+        return false
+    }
+
+    // Check if there are any coins between current square and target square
+    for (i in (targetSquare + 1)..<position) {
+        if (coins[i] != " ") {
+            println("Invalid move. You cannot jump over another coin.")
+            return false
+        }
+    }
+
+    // Move the coin to the new square
+    coins[targetSquare] = coins[position]
+    coins[position] = " "
+    return true
 }
